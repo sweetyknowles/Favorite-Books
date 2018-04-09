43d7294eceebcedb64b8d66bfdf621c0e8a41d95
SweetyKnowles <sweetyjames@hotmail.co.uk>
2018-04-09 18:53:58 -0400

    added second route

--- app/controllers/api/readers_controller.rb
+++ app/controllers/api/readers_controller.rb
@@ -2,10 +2,18 @@ class Api::ReadersController < ApplicationController
 
     def index 
         @readers = Reader.all
-        render json: @ readers 
+        render json: @readers 
+    end
+
+    def create 
+        @reader = Reader.create!(reader_params)
+        render json: @reader
     end
     
-
+    def show 
+        @reader = Reader.find(params[:id])
+        render json:@reader
+    end
 
 
 
