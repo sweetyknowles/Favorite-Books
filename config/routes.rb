c5e3bd212f274f2259542c63a8c8eb31e5326fb5
SweetyKnowles <sweetyjames@hotmail.co.uk>
2018-04-09 18:31:41 -0400

    added the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :readers do
+      resources :books
+    end
+  end
 end
+
+
