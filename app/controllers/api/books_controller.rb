f0f6654a570f036a6fc28f405b173f6b7eb57173
SweetyKnowles <sweetyjames@hotmail.co.uk>
2018-04-09 20:09:00 -0400

    edited the second model

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,39 +1,39 @@
 class Api::BooksController < ApplicationController
 
     def index
-        @artists = Artist.all
-        render json: @artists
+        @books = Book.all
+        render json: @books
       end
     
       def create
-        @artist = Artist.create!(artist_params)
+        @book = Book.create!(book_params)
     
-        render json: @artist
+        render json: @book
       end
     
       def show
-        @artist = Artist.find(params[:id])
+        @book = Book.find(params[:id])
     
-        render json: @artist
+        render json: @book
       end
     
       def update
-        @artist = Artist.find(params[:id])
-        @artist.update!(artist_params)
+        @book = Book.find(params[:id])
+        @book.update!(book_params)
     
-        render json: @artist
+        render json: @book
       end
     
       def destroy
-        @artist = Artist.find(params[:id]).delete
+        @book = Book.find(params[:id]).delete
     
         render status: :ok
       end
     
       private
     
-      def artist_params
-        params.require(:artist).permit(:name, :photo_url, :nationality)
+      def book_params
+        params.require(:book).permit(:title,:author, :publish, :genre, :photo_url, :synopis)
       end
     end
 end
