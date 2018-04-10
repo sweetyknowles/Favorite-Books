c94e856577bc4cd88e5354a5c53aff3a9deb5faf
SweetyKnowles <sweetyjames@hotmail.co.uk>
2018-04-09 20:51:57 -0400

    added app file to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import ReaderList from "./components/ReaderList"
+import Reader from "./components/Reader"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Router>
+            <div className="App">
+
+                <div>
+                    <h1>Reading Bee</h1>
+                    <div>
+                        <div><Link to="/">All Reading Bees</Link></div>
+                    </div>
+                </div>
+
+                <Route exact path="/" component={ReaderList}/>
+                <Route path="/reader/:id" component={Reader}/>
+
+            </div>
+        </Router>
     );
-  }
 }
+}
+
 
 export default App;
